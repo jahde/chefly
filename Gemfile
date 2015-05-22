2db2527d9a1e12d45a0db18131f567c348105c01
sim1hill <simone.c.hill@gmail.com>
2015-05-22 18:17:43 -0400

    day one done

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'dotenv-rails'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.0'
@@ -41,6 +42,7 @@ end
 
 gem 'pg'
 gem 'httparty'
+
 
 group :test, :development do
   gem 'rspec-rails'
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,8 +3,18 @@ class Restaurant < ActiveRecord::Base
 
 
   def self.menu_api
-    response = HTTParty.get('http://api.locu.com/v1_0/venue/search/?api_key=4ce3d82c3063477c85dd0b1926750d78ed9de51d&postal_code=10009')
-      binding.pry
+    
+    response = HTTParty.get("http://api.locu.com/v1_0/venue/search/?api_key=ENV[API_KEY]&postal_code=10009&has_menu=true")
+      response["objects"].each do |restaurant|
+      if (restaurant["has_menu"] == true) && restaurant["categories"].include?("restaurant")
+        name = restaurant["name"]
+        location = restaurant["street_address"]
+        Restaurant.create(name: name,location: location)
+        menu_id = restaurant["id"]
+        menus = HTTParty.get("http://api.locu.com/v1_0/venue/#{menu_id}/?api_key=ENV[API_KEY]")
+      end 
+      end
 
   end
 end
+
