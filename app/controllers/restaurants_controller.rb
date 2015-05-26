ab2e21d64aff1bc852e41d27d31acb1747250d15
hidace <dacewillmott@gmail.com>
2015-05-26 17:06:10 -0400

    add production to database yaml

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,6 +1,6 @@
 class RestaurantsController < ApplicationController
   def index
-    Restaurant.data_dump
+    #Restaurant.data_dump
     #please run this! 
     narrowed_restaurants = []
     Restaurant.all.each do |restaurant|
