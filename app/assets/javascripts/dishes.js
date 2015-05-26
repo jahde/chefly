6813c0e8857226d7a32cc931a3703fb13abe42b8
jahde <jahde.hiywan@gmail.com>
2015-05-26 14:16:31 -0400

    work on image size

---
+++ app/assets/javascripts/dishes.js
@@ -0,0 +1,48 @@
+// test
+$(document).ready(function(){
+  function getRecipeJson() {
+          var apiKey = "dvxBm9UtLmVZby8Qvc1IO3i52o9Y1Dsj";
+          // var titleKeyword = "latin egg wrap";
+          var titleKeyword = $('h1').text().toLowerCase();
+          var url = "http://api.bigoven.com/recipes?pg=1&rpp=25&any_kw="
+                    + titleKeyword
+                    + "&api_key="+apiKey;
+          $.ajax({
+              type: "GET",
+              dataType: 'json',
+              cache: false,
+              url: url,
+              success: function (data) {
+                  var recipe_list = [];
+                  for(var i = 0; i < 5; i++){
+                    recipe_list.push(data.Results[i]["RecipeID"]);
+                    $("#dish-dish .thumbnail").append(data.Results[i]["Title"]+"<br>");
+                    var source = data.Results[i]["HeroPhotoUrl"];
+                    $("#dish-dish .thumbnail").append("<img src="+source+" />");
+                  }
+                  recipe_list.forEach(function(entry){
+                    // fetchRecipeJson(entry);
+                  })
+              }
+          });
+      };
+      getRecipeJson();
+
+      function fetchRecipeJson(iD) {
+      var apiKey = "dvxBm9UtLmVZby8Qvc1IO3i52o9Y1Dsj";
+      var recipeID = iD;
+      var url = "http://api.bigoven.com/recipe/" + recipeID + "?api_key="+apiKey;
+      $.ajax({
+               type: "GET",
+               dataType: 'json',
+               cache: false,
+               url: url,
+               success: function (data) {
+                  // $("#dish-dish").html(data.Instructions);
+                  // debugger;
+                  // $("#dish-dish").html(data.Ingredients[0]["Name"])
+                  }
+               });
+             }
+  });
+
--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,10 @@
 class RestaurantsController < ApplicationController
   def index
-    Restaurant.data_dump
+    # Restaurant.data_dump
     narrowed_restaurants = []
     Restaurant.all.each do |restaurant|
       if restaurant.dishes.count > 0
-          narrowed_restaurants.push(restaurant) 
+          narrowed_restaurants.push(restaurant)
       end
     end
     @restaurants = narrowed_restaurants
