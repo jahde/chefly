c954d724333d5605a4f6dcf8587db6d133919865
jahde <jahde.hiywan@gmail.com>
2015-06-02 15:38:30 -0400

    add flexslider js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,6 @@
 //= require jquery_ujs
 //= require bootstrap
 //= require plugins.min
+//= require jquery.flexslider-min
 //= require_tree ../../../vendor/assets/javascripts/.
 //= require_tree .
